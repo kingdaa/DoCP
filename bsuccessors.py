a9920bf81dd288dee940ca4309efb3d1a97bd973
kingdaa <kinghuang90@gmail.com>
2014-11-24 16:10:02 -0800

    .

--- bsuccessors.py
+++ bsuccessors.py
@@ -62,5 +62,29 @@ def test():
     assert bridge_problem(frozenset((1, 2, 5, 10), ))[-1][-1] == 17
     return 'tests pass'
 
+class TestBridge: """
+>>> elapsed_time(bridge_problem([1,2,5,10]))
+17
+
+## There are two equally good solutions
+>>> S1 = [(2, 1, '->'), (1, 1, '<-'), (5, 10, '->'), (2, 2, '<-'), (2, 1, '->')]
+>>> S2 = [(2, 1, '->'), (2, 2, '<-'), (5, 10, '->'), (1, 1, '<-'), (2, 1, '->')]
+>>> path_actions(bridge_problem([1,2,5,10])) in (S1, S2)
+True
+
+## Try some other problems
+>>> path_actions(bridge_problem([1,2,5,10,15,20]))
+[(2, 1, '->'), (1, 1, '<-'), (10, 5, '->'), (2, 2, '<-'), (2, 1, '->'), (1, 1, '<-'), (15, 20, '->'), (2, 2, '<-'), (2, 1, '->')]
+
+>>> path_actions(bridge_problem([1,2,4,8,16,32]))
+[(2, 1, '->'), (1, 1, '<-'), (8, 4, '->'), (2, 2, '<-'), (1, 2, '->'), (1, 1, '<-'), (16, 32, '->'), (2, 2, '<-'), (2, 1, '->')]
+
+>>> [elapsed_time(bridge_problem([1,2,4,8,16][:N])) for N in range(6)]
+[0, 1, 2, 7, 15, 28]
+
+>>> [elapsed_time(bridge_problem([1,1,2,3,5,8,13,21][:N])) for N in range(8)]
+[0, 1, 1, 2, 6, 12, 19, 30]
+
+"""
 
 print test()
